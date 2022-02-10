5677b340e72628a48f79edb730cbfaa398332cb8
Ricardo Amoedo <78619036+ricardoamoedo@users.noreply.github.com>
2022-02-10 18:59:07 -0300

    criação de métodos e alteração do Web Scraping

--- config/db.py
+++ config/db.py
@@ -1,13 +1,33 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="us-cdbr-east-05.cleardb.net",
-  user="b6d5367ae4563a",
-  password="d3fd5ed9",
-  database="heroku_f831c411194bbea"
-)
+def open_db():
+  db_ = mysql.connector.connect(
+    host="us-cdbr-east-05.cleardb.net",
+    user="b6d5367ae4563a",
+    password="d3fd5ed9",
+    database="heroku_f831c411194bbea"
+  )
 
-mycursor = mydb.cursor()
+  cursor_ = db_.cursor(prepared=True)
 
-def create_article():
-    sql = "INSERT INTO articles (db_title, db_url) VALUES (%s, %s)"
+  return True
+
+def create_article(id: int, featured: str=" ", title: str=" ", url:str=" ", imageUrl:str=" ", newsSite:str=" ", summary:str=" ", 
+publishedAt:str=" ", launches:str=" ", events:str=" "):
+    if id !=0:
+
+      open_db()
+
+      sql = "INSERT INTO articles (db_id, db_featured, db_title, db_url, db_imageUrl, db_newsSite, db_summary, db_publishedAt, db_launches, db_events) VALUES (?,?,?,?,?,?,?,?,?,?)"
+      var = (id, featured, title, url, imageUrl, newsSite, summary, publishedAt, str(launches), str(events))
+  
+      cursor_.execute(sql, var)
+
+      db_.commit()
+
+      db_.close()
+
+      return {"status": 200, "Message": "done!"}
+    
+    return{"status": 404, "Message": "Error!"}
+
--- webscraping.py
+++ webscraping.py
@@ -1,8 +1,18 @@
+from config.db import open_db, create_article
 import requests
-from bs4 import BeautifulSoup
+import mysql.connector
+from sqlalchemy import create_engine
 
 url = 'https://api.spaceflightnewsapi.net/v3/articles/'
 
 page = requests.get(url) 
 
-for n in (page.json()): print (n['id'], "    ", (n['title']))
+db_ = mysql.connector.connect(
+  host="us-cdbr-east-05.cleardb.net",
+  user="b6d5367ae4563a",
+  password="d3fd5ed9",
+  database="heroku_f831c411194bbea"
+)
+
+for n in (page.json()): 
+  create_article(int(n['id']),n['featured'], n['title'], n['url'], n['imageUrl'], n['newsSite'], n['summary'], n['publishedAt'], n['launches'], n['events'])
